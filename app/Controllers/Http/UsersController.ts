44ca6be68abd65a4238369d69fe62996b9cccdb6
Fajar Putra <106464666+fajarph@users.noreply.github.com>
2023-09-29 21:39:10 +0700

    create get users include blog post

---
+++ app/Controllers/Http/UsersController.ts
@@ -0,0 +1,27 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+
+export default class UsersController {
+
+    public async getUsers({ response, auth }: HttpContextContract) {
+        try {
+            await auth.use("api").authenticate()
+
+            const user = await User.query()
+                .preload("blog_posts", (query) => {
+                    query.select("id", "title", "content", "title")
+                })
+                .select("id", "name", "email", "gender")
+
+            response.status(200).json({
+                status: 200,
+                user: user
+            })
+        } catch (error) {
+            response.status(401).json({
+                status: 401,
+                msg : error.message
+            })
+        }
+    }
+}
